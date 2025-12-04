b9a5a77ee766fade7466ff5a6c4e941921a8c4a8
giuseppe.monaco <giuseppemonaco97@gmail.com>
2025-12-04 16:51:03 +0000

    Add files via upload

--- UNIT_1/S2/L4/geometria.py
+++ UNIT_1/S2/L4/geometria.py
@@ -9,17 +9,30 @@ def perimetro():
     if scelta == 1:
         print("Hai selezionato il quadrato!")
         lato =float(input("Inserisci il valore del lato del quadrato: "))
-        print(f"Il perimetro del quadrato, avente lato {lato} è: ", lato*4)
+        perimetroq=float(lato*4)
+        return perimetroq, scelta
     elif scelta == 2:
         print("Hai selezionato il perimetro del rettangolo")
         base = float(input("Inserisci il valore della base: "))
         altezza = float(input("Inserisci il valore dell'altezza "))
-        print (f"Il perimetro del rettangolo, avente base {base} e altezza {altezza} è: ", base*2 + altezza*2)
+        perimetror=float(base*2+altezza*2)
+        return perimetror, scelta
     elif scelta == 3:
         print("Hai selezionato la circonferenza del cerchio")
         raggio = float(input("Inserisci il valore del raggio: "))
-        print(f"La circonferenza del cerchio, avente raggio {raggio} è: ", raggio*3.14)
+        circonferenza=float(raggio*2*3.14)
+        return circonferenza, scelta
     else:
         print("Digitare '1', '2' o '3'")
+        return None
 
-perimetro()
+risultato, scelta = perimetro()
+
+if risultato is not None:
+    if scelta == 3:
+        print("La circonferenza del cerchio è:", risultato)
+    else:
+        print("Il perimetro della figura è:", risultato)
+
+
+          
